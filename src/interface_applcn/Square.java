65faac6dbd0c3502d81d0db6fae358f69846a8a0
Vanshaj Rathi <Vanshaj Rathi@DESKTOP-4Q6NA7A>
2023-05-31 11:39:56 +0530

    commit in JBDL

--- src/interface_applcn/Square.java
+++ src/interface_applcn/Square.java
@@ -2,6 +2,16 @@ package interface_applcn;
 
 public class Square implements Area {
   int l = 10;
+  
+  public Square() {
+	  this(4);
+	  System.out.println("Square with no parameter called ");
+  }
+  
+  public Square(int a) {
+	  this.l = a;
+	  System.out.println(l);
+  }
   	@Override
 	public void area() {
 		// TODO Auto-generated method stub
